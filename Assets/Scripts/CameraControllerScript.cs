ab29d0adb1075664ee9e9a2699bbd25363652ecf
boxingjellyfish <boxingjellyfish@noreply.com>
2017-07-04 22:37:01 -0300

    initial commit

---
+++ Assets/Scripts/CameraControllerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerScript : MonoBehaviour {
+
+    public float moveSpeed;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    public float zoomSpeed;
+    public float zoomMax;
+    public float zoomMin;
+
+    public float lerpSpeed;
+
+    private Vector3 nextPosition;
+
+    // Use this for initialization
+    void Start ()
+    {
+        nextPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        nextPosition.x += Input.GetAxis("Horizontal") * moveSpeed;
+        nextPosition.x = nextPosition.x < minX ? minX : nextPosition.x;
+        nextPosition.x = nextPosition.x > maxX ? maxX : nextPosition.x;
+
+        nextPosition.y += Input.GetAxis("Vertical") * moveSpeed;
+        nextPosition.y = nextPosition.y < minY ? minY : nextPosition.y;
+        nextPosition.y = nextPosition.y > maxY ? maxY : nextPosition.y;
+        
+        nextPosition.z += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        nextPosition.z = nextPosition.z < zoomMin ? zoomMin : nextPosition.z;
+        nextPosition.z = nextPosition.z> zoomMax ? zoomMax : nextPosition.z;
+
+        if(Input.GetButton("Fire3"))
+        {
+            nextPosition = new Vector3(0, 0, -10);
+        }
+        
+        transform.position = Vector3.Lerp(transform.position, nextPosition, Time.deltaTime * lerpSpeed);
+    }
+}
---
+++ Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour
+{
+    public Transform squarePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void ExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    public void AddRectangleClick()
+    {
+        var newSquare = Instantiate(squarePrefab, new Vector3(Random.Range(-25, 25), Random.Range(-20, 20), 0), Quaternion.identity);
+        newSquare.transform.parent = GameObject.Find("_Dynamic").transform;
+        /*
+        GameObject shape = new GameObject();
+        shape.transform.parent = GameObject.Find("_Dynamic").transform;
+        shape.transform.position = new Vector3(Random.Range(0,10), Random.Range(0, 10), 0);
+
+        Color c1 = Color.yellow;
+        Color c2 = Color.red;
+        int lengthOfLineRenderer = 20;
+
+        LineRenderer lineRenderer = shape.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = lengthOfLineRenderer;
+
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+            );
+        lineRenderer.colorGradient = gradient;
+        */
+    }
+}
